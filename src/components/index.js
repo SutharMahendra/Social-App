46c875284ae7cbb4c59fd9b961d5f91c300f2fcc
SutharMahendra <omajimahendra@gmail.com>
2025-01-18 11:50:58 +0530

    common input created

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from './container/Container'
+import Button from "./Button";
 
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
-    Container
+    Container,
+    Button
 }
